3515ba2e2d2d67f289d76f5a7015a81bdbdd5bac
pancham01 <108358732+pancham01@users.noreply.github.com>
2026-08-09 11:41:27 +0530

    update

--- src/main/java/learning/spring/mvc/controller/WelcomeController.java
+++ src/main/java/learning/spring/mvc/controller/WelcomeController.java
@@ -1,37 +1,32 @@
 package learning.spring.mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-//@RequestMapping("/api1")
 public class WelcomeController {
 
-	
 	@RequestMapping("/")
-	public String hello()
-	{
+	public String hello() {
 		System.out.println("WelcomeController.hello()");
 		return "index11";
 	}
-	
 
-	@RequestMapping("/welcome")
-	public String greet()
-	{
-		System.out.println("WelcomeController.greet()");
-		return "welcome";
+	@RequestMapping("/getData")
+	@ResponseBody
+	public String queryParameter(@RequestParam(name = "user", defaultValue = "deafult-User") String username) {
+		System.out.println("Welcome Mr." + username);
+		return "Welcome Mr." + username;
 	}
-	
-	@RequestMapping("/admin")
-	public String adminLogIn()
-	{
-		System.out.println("WelcomeController.adminLogIn()");
-		return "redirect:/welcome";
+
+	@RequestMapping("/getUser/{id}")
+	@ResponseBody
+	public String getUserById(@PathVariable(name = "id") Long id) {
+		System.out.println("WelcomeController.getUserById : " + id);
+		return "Hi Mr. This is your Profile information and your User id is : " + id;
 	}
-	
-	
+
 }
